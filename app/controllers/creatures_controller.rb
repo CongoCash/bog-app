12f59d60e9aaef3e488a96ec75d3db64e75bcd60
David <davjue@gmail.com>
2017-09-18 02:24:15 -0700

    finished

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -20,6 +20,38 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
   end
 
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+
+  def update
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    creature = Creature.find_by_id(creature_id)
+
+    # update the creature
+    creature.update_attributes(creature_params)
+
+    # redirect to show page for the updated creature
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature_id = params[:id]
+
+    # use `creature_id` to find the creature in the database
+    # and save it to a variable
+    creature = Creature.find_by_id(creature_id)
+
+    # destroy the creature
+    creature.destroy
+
+    # redirect to creatures index
+    redirect_to creatures_path
+  end
+
   private
   def creature_params
     params.require(:creature).permit(:name, :description)
